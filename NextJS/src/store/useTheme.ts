6fc9ce77e0a9fb4e68d0e7936607d6db2b0940e4
Jorge <jrg1011@alu.ubu.es>
2021-11-03 19:13:56 +0100

    Persistent Theme

--- NextJS/src/store/useTheme.ts
+++ NextJS/src/store/useTheme.ts
@@ -1,15 +1,37 @@
 import create from "zustand";
 import { Theme } from "enums/Theme";
-import persist from "zustand/middleware";
+import { persist } from "zustand/middleware";
+
 /**
  * Updates the current tailwind theme by appending it to the html body.
  * @param theme New theme enum to replace with.
+ * @returns Current / new theme
  */
-function setTheme(theme: Theme) {
+function setTheme(theme: Theme): Theme {
   const root = window.document.documentElement;
   root.classList.remove(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
   root.classList.add(theme);
   localStorage.setItem("theme", theme);
+  return theme;
+}
+
+/**
+ * Loads the current saved theme
+ * @returns {any}
+ */
+function loadSavedTheme(): Theme {
+  let theme = Theme.DARK;
+  if (typeof window === "undefined") {
+    return Theme.DARK;
+  }
+
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === null || savedTheme == Theme.DARK) {
+    return setTheme(Theme.DARK);
+  } else {
+    return setTheme(Theme.LIGHT);
+  }
 }
 
 interface IThemeStore {
@@ -17,9 +39,12 @@ interface IThemeStore {
   toggleTheme: () => void;
 }
 
+/**
+ * Store that manages the current theme
+ * @returns {IThemeStore} A state consumer that returns the current theme and the theme toggler
+ */
 const useThemeStore = create<IThemeStore>((set, get) => {
-  const currentTheme = Theme.DARK;
-
+  const currentTheme = loadSavedTheme();
   const toggleTheme = () => {
     const { currentTheme } = get();
 
