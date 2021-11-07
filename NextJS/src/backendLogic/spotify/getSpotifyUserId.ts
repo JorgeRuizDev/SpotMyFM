526d564057a8bea39e9f653138a8a3c3f6379346
Jorge Ruiz <jorgelinirg@gmail.com>
2021-11-07 12:44:31 +0100

    Added spotify token to userID

--- NextJS/src/backendLogic/spotify/getSpotifyUserId.ts
+++ NextJS/src/backendLogic/spotify/getSpotifyUserId.ts
@@ -14,6 +14,7 @@ export async function getSpotifyUserId(
 
   try {
     // Configure the api
+    // TODO: Replace the object with the improved one
     const api = new SpotifyWebApi();
     api.setAccessToken(spotifyApiToken);
 
