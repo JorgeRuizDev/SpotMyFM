312f74c85620eb9282c8ac97191a0e7154c8b121
Jorge Ruiz <jorgelinirg@gmail.com>
2021-11-07 12:40:58 +0100

    Spotify User backend validation

---
+++ NextJS/src/backendLogic/spotify/getSpotifyUserId.ts
@@ -0,0 +1,28 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+/**
+ * Given a spotify api token, get the api token user Id.
+ * @param spotifyApiToken Spotify Token
+ * @returns
+ */
+export async function getSpotifyUserId(
+  spotifyApiToken: string = ""
+): Promise<[string | null, any]> {
+  if (spotifyApiToken === null || spotifyApiToken.length === 0) {
+    return ["", "Empty Token!"];
+  }
+
+  try {
+    // Configure the api
+    const api = new SpotifyWebApi();
+    api.setAccessToken(spotifyApiToken);
+
+    // Get the user and return
+    const user = await api.getMe();
+    return [user.body.id, null];
+  } catch (e) {
+    // TODO: Parse the error
+
+    return [null, e];
+  }
+}
