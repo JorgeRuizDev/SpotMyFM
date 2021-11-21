c55fc57b54e030da664fe98a9bef2209378fc6ce
Jorge Ruiz <jorgelinirg@gmail.com>
2021-11-21 01:11:29 +0100

    Added a Toggle Switch Component

---
+++ NextJS/src/components/core/input/atoms/Switch/Switch.styles.ts
@@ -0,0 +1,14 @@
+import tw from "twin.macro";
+
+const Inline = tw.div`
+	flex
+	flex-row
+	items-center
+
+	space-x-4
+	m-1
+`;
+
+const Styled = { Inline };
+
+export default Styled;
---
+++ NextJS/src/components/core/input/atoms/Switch/Switch.tsx
@@ -0,0 +1,40 @@
+import Sw from "react-switch";
+import Styled from "./Switch.styles";
+interface ISwitchProps {
+  isChecked: boolean;
+  onToggle: () => void;
+  disabled?: boolean;
+  children?: React.ReactNode | React.ReactNode[];
+}
+
+function Switch({
+  isChecked,
+  onToggle: toggleAction,
+  children,
+  disabled
+}: ISwitchProps) {
+  return (
+    <Styled.Inline>
+      <Sw
+        checked={isChecked}
+        onChange={toggleAction}
+        // Checked
+        onColor="#A7F3D0"
+        onHandleColor="#10B981"
+        // Unchecked:
+        offColor="#BFDBFE"
+        handleDiameter={17}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        disabled={disabled || false}
+        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+        height={17}
+        width={35}
+      />
+      {children}
+    </Styled.Inline>
+  );
+}
+
+export default Switch;
