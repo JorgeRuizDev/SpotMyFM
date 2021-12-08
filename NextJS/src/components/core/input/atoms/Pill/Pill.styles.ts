e88b304f11b5eb127fd036627c8d22582982f48f
Jorge Ruiz <jorgelinirg@gmail.com>
2021-12-08 20:53:28 +0100

    Added Pill Component

---
+++ NextJS/src/components/core/input/atoms/Pill/Pill.styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+interface IPill {
+  typeColor: string;
+}
+
+const Pill = styled.button<IPill>(({ typeColor }) => [
+  typeColor === "tag" &&
+    tw`
+		bg-red-500
+		hover:bg-red-400
+	`,
+
+  typeColor === "genre" &&
+    tw`
+		bg-blue-400
+		hover:bg-blue-500
+	`,
+
+  typeColor === "artist" &&
+    tw`
+	bg-lightGreen-base
+	hover:bg-lightGreen-hover
+`,
+  typeColor === "MySpotifyFmTags" &&
+    tw`
+    bg-green-100
+    text-green-700
+    border-green-700
+    border
+    hover:bg-green-200
+    hover:text-green-600
+  
+  `,
+]);
+
+const Styled = { Pill };
+
+export default Styled;
---
+++ NextJS/src/components/core/input/atoms/Pill/Pill.test.tsx
@@ -0,0 +1,20 @@
+import { fireEvent, render } from "@testing-library/react";
+import React from "react";
+import Pill from "./Pill";
+
+describe("<Pill />", () => {
+  test("Renders the component", () => {
+    const closeFn = jest.fn();
+    const component = render(
+      <Pill onClose={closeFn} type={""}>
+        Test
+      </Pill>
+    );
+
+    const btn = component.getByText("Test");
+
+    fireEvent(btn, new MouseEvent("click"));
+
+    expect(closeFn).toBeCalled();
+  });
+});
