e0e7cd9867bec5b32d2b791e79facdcbccca8042
Jorge Ruiz <jorgelinirg@gmail.com>
2021-12-26 20:25:57 +0100

    Updated Transition Animation

--- NextJS/src/components/core/display/molecule/Transition/Transition.styles.ts
+++ NextJS/src/components/core/display/molecule/Transition/Transition.styles.ts
@@ -4,22 +4,22 @@ import tw from "twin.macro";
 
 const Transition = styled(_Transition)`
   &.enter {
-    ${tw`transition ease-out duration-1000`}
+    ${tw`transition ease-out duration-200`}
   }
   &.enterFrom {
-    ${tw`transform scale-95 opacity-0`}
+    ${tw`opacity-0 translate-y-1`}
   }
   &.enterTo {
-    ${tw`transform scale-100 opacity-100`}
+    ${tw`opacity-100 translate-y-0`}
   }
   &.leave {
-    ${tw`transition duration-1000 ease-out`}
+    ${tw`transition ease-in duration-150`}
   }
   &.leaveFrom {
-    ${tw`transform scale-100 opacity-100`}
+    ${tw`opacity-100 translate-y-0`}
   }
   &.leaveTo {
-    ${tw`transform scale-95 opacity-0`}
+    ${tw`opacity-0 translate-y-1`}
   }
 `
 const Styled = {Transition};
