fe58b838e33d1484cbf48091192de24080ed668a
Jorge Ruiz <jorgelinirg@gmail.com>
2021-12-27 23:42:35 +0100

    Outdated Cache Prompt

---
+++ NextJS/src/components/core/notification/notificationContents/CacheOutdatedMessage/CacheOutdatedMessage.tsx
@@ -0,0 +1,39 @@
+import { useMemo } from "react";
+import Styled from "./CacheOutdatedMessage.styles";
+interface ICacheOutdatedMessageProps {
+  onClick: () => void;
+  lastCacheUpdate?: Date;
+}
+
+function CacheOutdatedMessage({
+  onClick,
+  lastCacheUpdate,
+}: ICacheOutdatedMessageProps) {
+
+  console.log(typeof lastCacheUpdate)
+
+  const update = useMemo(
+    () => lastCacheUpdate?.toLocaleDateString(),
+    [lastCacheUpdate]
+  );
+  return (
+    <Styled.NotificationWrapper>
+      <div>
+        <Styled.b>
+          The local library cache seems to be outdated{" "}
+          {update && `(Last Update ${update})`}{" "}
+        </Styled.b>
+
+        <br />
+        <Styled.p>
+          ¿Do you want to refresh the Cache? This process will only add your new
+          liked tracks.
+        </Styled.p>
+      </div>
+
+      <Styled.CacheButton onClick={onClick}>Refresh Cache</Styled.CacheButton>
+    </Styled.NotificationWrapper>
+  );
+}
+
+export default CacheOutdatedMessage;
