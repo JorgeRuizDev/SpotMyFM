c8a62274316c3016b88f905f16adbf9c8664e6a6
Jorge Ruiz <jorgelinirg@gmail.com>
2021-12-28 20:54:15 +0100

    Added Tailwind auto scroll / marquee effect

--- NextJS/tailwind.config.js
+++ NextJS/tailwind.config.js
@@ -21,6 +21,20 @@ module.exports = {
     },
     extend: {
       colors: {
+        animation: {
+          marquee: "marquee 25s linear infinite",
+          marquee2: "marquee2 25s linear infinite",
+        },
+        keyframes: {
+          marquee: {
+            "0%": { transform: "translateX(0%)" },
+            "100%": { transform: "translateX(-100%)" },
+          },
+          marquee2: {
+            "0%": { transform: "translateX(100%)" },
+            "100%": { transform: "translateX(0%)" },
+          },
+        },
         lightCard: {
           base: white,
           hover: coolGray[100],
