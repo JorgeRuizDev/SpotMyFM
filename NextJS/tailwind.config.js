bbde60ad7eba9da409a59da3feec411b343efd9f
Jorge Ruiz <jorgelinirg@gmail.com>
2021-12-29 00:23:16 +0100

    Added Tailwind Marquee Animation

--- NextJS/tailwind.config.js
+++ NextJS/tailwind.config.js
@@ -20,21 +20,22 @@ module.exports = {
       ],
     },
     extend: {
+      animation: {
+        marquee: "marquee 5s linear infinite",
+        marquee2: "marquee2 5s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+
       colors: {
-        animation: {
-          marquee: "marquee 25s linear infinite",
-          marquee2: "marquee2 25s linear infinite",
-        },
-        keyframes: {
-          marquee: {
-            "0%": { transform: "translateX(0%)" },
-            "100%": { transform: "translateX(-100%)" },
-          },
-          marquee2: {
-            "0%": { transform: "translateX(100%)" },
-            "100%": { transform: "translateX(0%)" },
-          },
-        },
         lightCard: {
           base: white,
           hover: coolGray[100],
