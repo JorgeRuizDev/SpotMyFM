5ba61b3f0d3f31d5a61ac7df54df8e4ca7cc29ab
Jorge Ruiz <jorgelinirg@gmail.com>
2021-12-30 00:24:01 +0100

    Improved Protected Route Behaviour

--- NextJS/src/pages/libraryManager.tsx
+++ NextJS/src/pages/libraryManager.tsx
@@ -1,16 +1,15 @@
-
-
 import ProtectedRoute from "components/core/wrappers/ProtectedRoute";
 import Head from "components/util/Head";
+import { ActivePage } from "enums/ActivePage";
 import React from "react";
+import { useSessionStore } from "store/useSession";
 
 export default function LibraryManager() {
+  //useSessionStore().setActivePage(ActivePage.LIBRARY_MGR);
   return (
     <>
       <Head subtitle={"Library Manager"} />
-      <ProtectedRoute onlyCached onlyLogged>
-        
-      </ProtectedRoute>
+      <ProtectedRoute onlyCached onlyLogged></ProtectedRoute>
     </>
   );
 }
