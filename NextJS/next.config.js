e11eb3b2156bdbfdc1db078aaa799a66126fde40
Jorge Ruiz <jorgelinirg@gmail.com>
2022-01-01 15:55:28 +0100

    Added Bundle Analyzer

--- NextJS/next.config.js
+++ NextJS/next.config.js
@@ -11,3 +11,14 @@ module.exports = {
     ],
   },
 };
+
+/**
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+});
+
+ */
