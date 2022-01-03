bb5999a98aa47d09c71ebb60b74e287118a8cacc
Jorge Ruiz <jorgelinirg@gmail.com>
2022-01-03 13:56:59 +0100

    Removed Buggy test

--- NextJS/src/components/core/display/molecules/Transition/Transition.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Transition from "./Transition";
-import { render } from "@testing-library/react";
-
-describe("<Transition />", () => {
-  test("Renders the component", () => {
-    const component = render(<Transition show={false} />);
-  });
-});
