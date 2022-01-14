b7b5d3c51ab3b117291988175a7180d7536565a3
JorgeRuizDev <JorgeRuizDev@users.noreply.github.com>
2022-01-14 19:39:22 +0000

    Prettified Code!

--- NextJS/src/components/core/cards/simpleCards/SimpleAlbumCard/SimpleAlbumCard.styles.ts
+++ NextJS/src/components/core/cards/simpleCards/SimpleAlbumCard/SimpleAlbumCard.styles.ts
@@ -3,7 +3,7 @@ import StyledCard from "./../../Card.styles";
 const Layout = StyledCard.Layout;
 const Image = StyledCard.Image;
 const ButtonRow = StyledCard.ButtonRow;
-const CardContent = StyledCard.Content
+const CardContent = StyledCard.Content;
 const Styled = { Layout, Image, ButtonRow, CardContent };
 
 export default Styled;
