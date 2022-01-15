23c3fc07ed2fe5beaa654c7fda0b2a1c7bed176f
Jorge Ruiz <jorgelinirg@gmail.com>
2022-01-15 21:00:35 +0100

    Added Dark Theme Text Color variant

--- NextJS/src/components/util/Link/Link.styles.ts
+++ NextJS/src/components/util/Link/Link.styles.ts
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
 
 const NextLink = tw(Link)`
@@ -7,12 +8,21 @@ const NextLink = tw(Link)`
 	color[inherit]
 `;
 
-const A = tw.a`
-	width[inherit]
-	height[inherit]
-	color[inherit]
-	hover:no-underline
-`;
+const A = styled.a(() => [
+  tw`
+    width[inherit]
+    height[inherit]
+    color[inherit]
+    dark:color[inherit]
+    hover:no-underline
+  `,
+
+  css`
+    .dark & {
+      color: inherit;
+    }
+  `,
+]);
 
 const Styled = { NextLink, A };
 
