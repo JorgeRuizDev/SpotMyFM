fff13e067d82ea8442504a20ef2ab142e5462876
Jorge Ruiz <jorgelinirg@gmail.com>
2022-01-15 20:44:26 +0100

    Added Active Page

--- NextJS/src/pages/albumManager.tsx
+++ NextJS/src/pages/albumManager.tsx
@@ -1,12 +1,21 @@
 import ProtectedRoute from "components/core/wrappers/ProtectedRoute";
 import AlbumManager from "components/pages/AlbumManager";
 import Head from "components/util/Head";
+import { ActivePage } from "enums/ActivePage";
+import { useEffect } from "react";
+import { useSessionStore } from "store/useSession";
 
 /**
  * Album Manager Page
  * @returns
  */
 export default function AlbumManagerPage(): JSX.Element {
+  const { setActivePage } = useSessionStore();
+
+  useEffect(() => {
+    setActivePage(ActivePage.ALBUM_MGR);
+  }, [setActivePage]);
+
   return (
     <ProtectedRoute onlyLogged>
       <Head subtitle="Album Manager" />
