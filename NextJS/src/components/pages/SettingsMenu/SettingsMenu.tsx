0b39502a35f56754cc3375747470aee91a84169a
Jorge Ruiz <jorgelinirg@gmail.com>
2022-01-23 20:58:30 +0100

    Added Settings Webpage

--- NextJS/src/components/pages/SettingsMenu/SettingsMenu.tsx
+++ NextJS/src/components/pages/SettingsMenu/SettingsMenu.tsx
@@ -1,8 +1,49 @@
 import Styled from "./SettingsMenu.styles";
+import Text from "styles/Text";
+
+import {
+  Tab,
+  TabContent,
+  TabContentWrap,
+  Tabs,
+} from "components/core/display/atoms/Tabs";
+import UserSettings from "./UserSettings";
+import CacheSettings from "./CacheSettings";
+import useTranslation from "next-translate/useTranslation";
+
 interface ISettingsMenuProps {}
 
 function SettingsMenu(props: ISettingsMenuProps): JSX.Element {
-  return <></>;
+  const { t } = useTranslation();
+
+  return (
+    <Styled.FullH>
+      <Text.Center>
+        <h1>{t("settings:settings")}</h1>
+      </Text.Center>
+
+      <Tabs defaultTabId={"1"}>
+        <Styled.TabLayout>
+          <Styled.TabWrap>
+            <Tab id="1" isColumn>
+              <p>{t("settings:user-settings")}</p>
+            </Tab>
+            <Tab id="2" isColumn>
+              <p>{t("settings:cache-settings")}</p>
+            </Tab>
+          </Styled.TabWrap>
+          <TabContentWrap>
+            <TabContent id={"1"}>
+              <UserSettings />
+            </TabContent>
+            <TabContent id={"2"}>
+              <CacheSettings />
+            </TabContent>
+          </TabContentWrap>
+        </Styled.TabLayout>
+      </Tabs>
+    </Styled.FullH>
+  );
 }
 
 export default SettingsMenu;
--- NextJS/src/pages/settings.tsx
+++ NextJS/src/pages/settings.tsx
@@ -1,3 +1,5 @@
+import SettingsMenu from "components/pages/SettingsMenu";
+import Head from "components/util/Head";
 import { ActivePage } from "enums/ActivePage";
 import { useEffect } from "react";
 import { useSessionStore } from "store/useSession";
@@ -5,5 +7,10 @@ import { useSessionStore } from "store/useSession";
 export default function Settings() {
   const setActivePage = useSessionStore().setActivePage;
   useEffect(() => setActivePage(ActivePage.SETTINGS), [setActivePage]);
-  return <></>;
+  return (
+    <>
+      <Head subtitle="Settings" />
+      <SettingsMenu />
+    </>
+  );
 }
