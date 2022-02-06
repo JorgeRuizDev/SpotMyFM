26c3784aff8a9b8a28dea83b8f719805081e2000
Jorge Ruiz <jorgelinirg@gmail.com>
2022-02-06 11:40:40 +0100

    Removed Mount Test

--- NextJS/src/components/core/cards/detailedCards/ArtistCompleteDetails/ArtistCompleteDetails.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ArtistCompleteDetails from "./ArtistCompleteDetails";
-import { render } from "@testing-library/react";
-
-describe("<ArtistCompleteDetails />", () => {
-  test("Renders the component", () => {
-    const component = render(<ArtistCompleteDetails />);
-  });
-});
