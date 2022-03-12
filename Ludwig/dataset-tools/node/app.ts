fc6bafb1a5be1f7f49cac04eaee17a413b4b2589
Jorge Ruiz <jorgelinirg@gmail.com>
2022-03-12 16:11:35 +0100

    Added Spotify Playlist Fix Command

--- Ludwig/dataset-tools/node/app.ts
+++ Ludwig/dataset-tools/node/app.ts
@@ -2,13 +2,16 @@ import SpotifyWebApi from "spotify-web-api-node";
 import { Command } from "commander";
 import { getPlaylist } from "./restQueries/getPlaylist";
 import { downloadAndSave } from "./util/saveToFolder";
+import {
+  updatePlaylistTrack,
+  isPreviewBroken,
+  findTrackWithPreview,
+} from "./util/fixPlaylistPreviews";
 const program = new Command();
 
 program
   .name("dataset-tools")
-  .description(
-    "CLI tool that manages "
-  );
+  .description("CLI tool that manages the Ludwig Dataset");
 
 program
   .command("spotify-downloader")
@@ -17,11 +20,75 @@ program
   .requiredOption("-t, --token <string>", "Spotify Token")
   .option("-o, --output <string>", "Output Directory Path", "./previews")
   .action((options) => {
-    main(options.token, options.uri, options.output)
-  }).parse()
+    downloadUri(options.token, options.uri, options.output);
+  });
+  
+program
+  .command("playlist-preview-fix")
+  .description(
+    "Updates the playlist so all the tracks have a preview attribute"
+  )
+  .requiredOption("-u, --uri <string>", "Spotify Playlist URI")
+  .requiredOption("-t, --token <string>", "Spotify Token")
+  .action((options) => {
+    console.info("Fixing Playlist Previews");
+
+    fixPlaylistPreviews(options.token, options.uri);
+  });
+if (!process.argv.length) {
+  program.help();
+}
+program.parse(process.argv);
 
 
-async function main(
+/**
+ * Given a playlist, updates all the tracks without a preview URL with a valid URL. 
+ * If no alternative was found, the track is removed from the playlist. 
+ * @param token A Valid Spotify Api Token
+ * @param uri  Playlist URI to clean. 
+ */
+async function fixPlaylistPreviews(token: string, uri: string) {
+  const api = new SpotifyWebApi();
+  api.setAccessToken(token);
+  const [_, type, id] = uri.split(":");
+  const playlist = await (await api.getPlaylist(id)).body;
+
+  const limit = 50;
+  let offset = 0;
+  const totalTracks = playlist.tracks.total;
+
+  // Get {limit} playlist items per page / request
+  while (offset < totalTracks) {
+    const tracks = await api.getPlaylistTracks(id, { limit, offset });
+    for (const t of tracks.body.items
+      .map((t) => t.track)
+      .filter((t) => !t.is_local && t.type === "track")) {
+
+      if (isPreviewBroken(t)) {
+        const alternative = await findTrackWithPreview(api, t);
+        updatePlaylistTrack(api, playlist, t, alternative);
+
+        if (!alternative) {
+          console.info(
+            `Track ${t.name} by ${t.artists[0].name} has no preview`
+          );
+        }
+      }
+    }
+
+    offset += limit;
+    console.info(`${Math.min(offset, totalTracks)}/${totalTracks}`);
+  }
+}
+
+/**
+ * This Function downloads all the tracks of a given URI into outPath
+ * Requires a valid spotify token
+ * @param token
+ * @param uri
+ * @param outPath
+ */
+async function downloadUri(
   token: string,
   uri: string,
   outPath: string = "./previews"
@@ -46,5 +113,4 @@ async function main(
 }
 
 if (require.main === module) {
-  
 }
---
+++ Ludwig/dataset-tools/node/util/fixPlaylistPreviews.ts
@@ -0,0 +1,53 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+function isPreviewBroken(track: SpotifyApi.TrackObjectSimplified) {
+  return !track.preview_url || track.preview_url.length == 0;
+}
+
+/**
+ * Function that replaces two tracks in one playlist.
+ * @param api Spotify API Object
+ * @param playlist Playlist to work with
+ * @param oldTrack Track to replace
+ * @param newTrack New Track (If null, oldTrack gets deleted from the playlist)
+ */
+async function updatePlaylistTrack(
+  api: SpotifyWebApi,
+  playlist: SpotifyApi.PlaylistBaseObject,
+  oldTrack: SpotifyApi.TrackObjectSimplified,
+  newTrack: SpotifyApi.TrackObjectSimplified | undefined | null
+) {
+  
+    await api.removeTracksFromPlaylist(playlist.id, [oldTrack]);
+
+    await api.addTracksToPlaylist(playlist.id, [newTrack.id]);
+}
+
+/**
+ * Find an alternative track that includes a preview
+ * @param api Spotify API Object
+ * @param track Spotify Track Object
+ * @returns A track with a preview, null if no alternative track was found.
+ */
+async function findTrackWithPreview(
+  api: SpotifyWebApi,
+  track: SpotifyApi.TrackObjectSimplified
+) {
+  if (!isPreviewBroken(track)) {  
+    return track;
+  }
+
+  const res = await api.searchTracks(`${track.name.split("-")[0]} + ${track.artists[0].name}`);
+  const fixed_candidates = res.body.tracks.items
+    .filter(
+      (t) => !isPreviewBroken(t) && t.artists[0].id == track.artists[0].id
+    )
+    .sort((a, b) => a.popularity - b.popularity);
+
+  if (fixed_candidates.length == 0) {
+    return null;
+  }
+  return fixed_candidates[0];
+}
+
+export { findTrackWithPreview, isPreviewBroken, updatePlaylistTrack };
