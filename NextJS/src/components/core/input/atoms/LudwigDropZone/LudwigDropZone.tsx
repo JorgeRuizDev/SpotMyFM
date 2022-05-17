8f9972eae4f89e27a7d5bc2151008521342d6250
JorgeRuizDev <JorgeRuizDev@users.noreply.github.com>
2022-05-17 19:28:46 +0000

    Prettified Code!

--- NextJS/src/components/core/input/atoms/LudwigDropZone/LudwigDropZone.tsx
+++ NextJS/src/components/core/input/atoms/LudwigDropZone/LudwigDropZone.tsx
@@ -103,8 +103,7 @@ function LudwigDropZone(props: ILudwigDropZoneProps): JSX.Element {
 
   useEffect(() => {
     if (isLogged) {
-
-      if (ids.length == 0){
+      if (ids.length == 0) {
         return;
       }
       const fn = async () => {
